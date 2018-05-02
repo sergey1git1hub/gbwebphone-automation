308fbc9fa6ae51e34747f1ca965b56f0f29f2988
SChubuk <nameserhiy@gmail.com>
2018-05-02 10:39:17 +0300

    launch-selenium-hub works launch-selenium-node works without exporting files

--- src/main/resources/batchScriptsRefactored/jenkins/hub/launch-selenium-hub.bat
+++ src/main/resources/batchScriptsRefactored/jenkins/hub/launch-selenium-hub.bat
@@ -1,4 +1,6 @@
 rem start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored\jenkins\hub\hubconfig.json"
 start /D "src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
+read
+pause
 
 
