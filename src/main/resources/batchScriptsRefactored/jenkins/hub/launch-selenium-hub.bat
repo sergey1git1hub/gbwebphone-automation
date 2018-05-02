72e0ea2637e145410305ae0b27045699e2eafca1
SChubuk <nameserhiy@gmail.com>
2018-05-02 10:54:25 +0300

    launch-selenium-hub works launch-selenium-node works without exporting files

--- src/main/resources/batchScriptsRefactored/jenkins/hub/launch-selenium-hub.bat
+++ src/main/resources/batchScriptsRefactored/jenkins/hub/launch-selenium-hub.bat
@@ -1,6 +1,8 @@
 rem start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored\jenkins\hub\hubconfig.json"
-start /B /D "src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
-read
-pause
+
+rem start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored\jenkins\hub\hubconfig.json"
+rem start /B /D "src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
+
+start /B /D "../.." java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
 
 
