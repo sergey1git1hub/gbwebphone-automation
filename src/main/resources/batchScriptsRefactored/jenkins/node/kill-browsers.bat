b4078d42a4dd860b39081957f4f6a98becc32449
SChubuk <nameserhiy@gmail.com>
2018-05-02 18:05:57 +0300

    kill browsers before and after suite.
    command line argument to batch file
    use relative path
    print output to jenkins console

--- src/main/resources/batchScriptsRefactored/jenkins/node/kill-browsers.bat
+++ src/main/resources/batchScriptsRefactored/jenkins/node/kill-browsers.bat
@@ -1,3 +1,3 @@
 cmdkey.exe /add:se-grid-node /user:se-grid-node\sergey /pass:123456
 psexec64.exe \\se-grid-node -s -i "c:\selenium-remote\kill-browsers.bat"
-cmdkey.exe /delete:se-grid-node
+cmdkey.exe /delete:se-grid-node 
