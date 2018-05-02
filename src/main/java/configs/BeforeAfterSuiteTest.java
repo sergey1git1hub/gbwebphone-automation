45f819e56b2a7552aaaa4841affd8c608f0825ef
SChubuk <nameserhiy@gmail.com>
2018-05-02 18:02:52 +0300

    kill drivers before and after suite.
    use relative path
    print output to jenkins console

--- src/main/java/configs/BeforeAfterSuiteTest.java
+++ src/main/java/configs/BeforeAfterSuiteTest.java
@@ -13,35 +13,42 @@ import java.io.File;
 public class BeforeAfterSuiteTest {
 
     SystemInfo systemInfo = new SystemInfo();
+    private String basePathInsideProject = "src/main/resources/batchScriptsRefactored";
+    private String location;
     private String killDriversHubScriptLocation;
     private String killDriversNodeScriptLocation;
+    private String killBrowsersHubScriptLocation;
+    private String killBrowsersNodeScriptLocation;
+
+
     private ProcessPrinter processPrinter = new ProcessPrinter();
 
 
     public BeforeAfterSuiteTest() throws Exception {
+        if(systemInfo.isLocal()){
+            location = "local";
+        } else {
+            location = "jenkins";
+        }
     }
 
     @BeforeSuite(alwaysRun=true)
     private void setup() throws Exception {
         System.out.println("BEFORE SUITE.");
         killDrivers();
+        killBrowsers();
     }
 
     @AfterSuite(alwaysRun=true)
     private void teardown() throws Exception {
         System.out.println("AFTER SUITE.");
         killDrivers();
+        killBrowsers();
 
     }
 
     private void killDrivers() throws Exception {
-        String basePathInsideProject = "src/main/resources/batchScriptsRefactored";
-        String location;
-        if(systemInfo.isLocal()){
-            location = "local";
-        } else {
-            location = "jenkins";
-        }
+
         killDriversHubScriptLocation = new File(basePathInsideProject +"/kill-drivers.bat").getCanonicalPath();
         killDriversNodeScriptLocation = new File(basePathInsideProject +
                 "/" + location + "/node/kill-drivers.bat").getCanonicalPath();
@@ -50,6 +57,19 @@ public class BeforeAfterSuiteTest {
         processPrinter.printProcessStream(killDriversHubProcess);
         Process killDriversNodeProcess = Runtime.getRuntime().exec(new String[]{killDriversNodeScriptLocation});
         processPrinter.printProcessStream(killDriversNodeProcess);
+    }
+
+    private void killBrowsers() throws Exception {
+
+        killBrowsersHubScriptLocation = new File(basePathInsideProject +"/kill-browsers.bat").getCanonicalPath();
+        killBrowsersNodeScriptLocation = new File(basePathInsideProject +
+                "/" + location + "/node/kill-browsers.bat").getCanonicalPath();
+
+        Process killDriversHubProcess = Runtime.getRuntime().exec(new String[]{killBrowsersHubScriptLocation});
+        processPrinter.printProcessStream(killDriversHubProcess);
+        Process killDriversNodeProcess = Runtime.getRuntime().exec(new String[]{killBrowsersNodeScriptLocation});
+        processPrinter.printProcessStream(killDriversNodeProcess);
+
     }
 
 }
