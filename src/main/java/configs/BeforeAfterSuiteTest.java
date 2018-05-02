a3ae6f1bb73f9ce0bbbf3aaa20801ec03820185c
SChubuk <nameserhiy@gmail.com>
2018-05-02 16:52:24 +0300

    launch hub on jenkins launch node on jenkins
    use relative path
    show node console
    print output to jenkins console

--- src/main/java/configs/BeforeAfterSuiteTest.java
+++ src/main/java/configs/BeforeAfterSuiteTest.java
@@ -1,6 +1,9 @@
 package configs;
 
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import utils.SystemInfo;
+
 import java.io.File;
 
 
@@ -9,17 +12,40 @@ import java.io.File;
  */
 public class BeforeAfterSuiteTest {
 
-    private String killDriversHubScriptLocation = new File("src\\main\\resources\\batchScripts\\drivers\\kill-drivers.bat").getCanonicalPath();
-    private String killDriversNodeScriptLocation = new File("src\\main\\resources\\batchScripts\\drivers\\kill-drivers-node.bat").getCanonicalPath();
+    SystemInfo systemInfo = new SystemInfo();
+    private String killDriversHubScriptLocation;
+    private String killDriversNodeScriptLocation;
     private ProcessPrinter processPrinter = new ProcessPrinter();
 
 
     public BeforeAfterSuiteTest() throws Exception {
     }
 
+    @BeforeSuite(alwaysRun=true)
+    private void setup() throws Exception {
+        System.out.println("BEFORE SUITE.");
+        killDrivers();
+    }
+
     @AfterSuite(alwaysRun=true)
     private void teardown() throws Exception {
         System.out.println("AFTER SUITE.");
+        killDrivers();
+
+    }
+
+    private void killDrivers() throws Exception {
+        String basePathInsideProject = "src/main/resources/batchScriptsRefactored";
+        String location;
+        if(systemInfo.isLocal()){
+            location = "local";
+        } else {
+            location = "jenkins";
+        }
+        killDriversHubScriptLocation = new File(basePathInsideProject +"/kill-drivers.bat").getCanonicalPath();
+        killDriversNodeScriptLocation = new File(basePathInsideProject +
+                "/" + location + "/node/kill-drivers.bat").getCanonicalPath();
+
         Process killDriversHubProcess = Runtime.getRuntime().exec(new String[]{killDriversHubScriptLocation});
         processPrinter.printProcessStream(killDriversHubProcess);
         Process killDriversNodeProcess = Runtime.getRuntime().exec(new String[]{killDriversNodeScriptLocation});
