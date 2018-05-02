f941fde77263d2c997956867b6c9bed7a28222c0
SChubuk <nameserhiy@gmail.com>
2018-05-02 12:13:48 +0300

    launch-selenium-hub works                        launch-selenium-node works                        without exporting files

--- src/main/resources/batchScriptsRefactored/jenkins/node/launch-selenium-node.bat
+++ src/main/resources/batchScriptsRefactored/jenkins/node/launch-selenium-node.bat
@@ -1,2 +1,1 @@
-cd src\main\resources\batchScriptsRefactored
-psexec.exe -i \\se-grid-node -s -u selenium-remote\sergey -p 123456  c:\selenium-remote\launch-selenium-node.bat
+start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" psexec.exe -i \\se-grid-node -s -u selenium-remote\sergey -p 123456  c:\selenium-remote\launch-selenium-node.bat
