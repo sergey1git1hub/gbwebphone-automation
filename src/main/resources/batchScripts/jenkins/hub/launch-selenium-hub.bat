d923746918a4aac063858e809ced8a227a10fdc3
SChubuk <nameserhiy@gmail.com>
2018-05-03 09:04:40 +0300

    batchScriptsRefactored -> batchScripts pulled files from remote PC's
    command line argument to batch file
    use relative path
    print output to jenkins console

--- src/main/resources/batchScripts/jenkins/hub/launch-selenium-hub.bat
+++ src/main/resources/batchScripts/jenkins/hub/launch-selenium-hub.bat
@@ -1,8 +1,1 @@
-start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored\jenkins\hub\hubconfig.json"
-
-rem start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScriptsRefactored\jenkins\hub\hubconfig.json"
-rem start /B /D "src\main\resources\batchScriptsRefactored" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
-
-rem start /D "../.." java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "jenkins/hub/hubconfig.json"
-
-
+start /D "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScripts" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "C:\Users\sergey\.jenkins\workspace\gbwebphone-automation\src\main\resources\batchScripts\jenkins\hub\hubconfig.json"
--- src/main/resources/batchScripts/local/hub/launch-selenium-hub.bat
+++ src/main/resources/batchScripts/local/hub/launch-selenium-hub.bat
@@ -1,3 +1,1 @@
 start /D "src\main\resources\batchScripts" java -jar selenium-server-standalone-3.11.0.jar -role hub -hubConfig "local/hub/hubconfig.json"
-
-
