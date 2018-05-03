488258b0642239d028f35fe63767b890ea93df60
SChubuk <nameserhiy@gmail.com>
2018-05-03 17:55:42 +0300

    Native service update.

---
+++ src/main/java/configs/NativeServiceUpdater.java
@@ -0,0 +1,56 @@
+package configs;
+
+import org.testng.annotations.Test;
+import utils.PropertiesLoader;
+import utils.SystemInfo;
+import java.io.File;
+
+
+public class NativeServiceUpdater {
+
+    ProcessPrinter processPrinter = new ProcessPrinter();
+    private String updateServiceHubScriptLocation;
+    private String updateServiceNodeScriptLocation;
+    private String basePathInsideProject = "src/main/resources/batchScripts";
+    private String location;
+    SystemInfo systemInfo = new SystemInfo();
+
+
+    public void updateNativeService() throws Exception {
+        if (Boolean.getBoolean("updateService")) {
+            updateNativeService(true);
+        }
+
+    }
+
+
+    public void updateNativeService(Boolean argument) throws Exception {
+        if(systemInfo.isLocal()){
+            location = "local";
+        } else {
+            location = "jenkins";
+        }
+        updateServiceHubScriptLocation = new File(basePathInsideProject +"/updatens.bat").getCanonicalPath();
+        updateServiceNodeScriptLocation = new File(basePathInsideProject +
+                "/" + location + "/node/updatens.bat").getCanonicalPath();
+
+        /*Process updateServiceHubProcess = Runtime.getRuntime().exec(new String[]{updateServiceHubScriptLocation});
+        processPrinter.printProcessStream(updateServiceHubProcess);*/
+        Process updateServiceNodeProcess = Runtime.getRuntime().exec(new String[]{updateServiceNodeScriptLocation});
+        processPrinter.printProcessStream(updateServiceNodeProcess);
+        System.out.println("Native service updated before running autotests.");
+    }
+
+    //independent of launching node and hub
+    @Test
+    private void updateNativeServiceTest() throws Exception {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        propertiesLoader.loadProperties();
+        NativeServiceUpdater nativeServiceUpdater = new NativeServiceUpdater();
+        nativeServiceUpdater.updateNativeService();
+    }
+
+
+
+
+}
